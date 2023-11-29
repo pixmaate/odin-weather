1a761ecc5129a211c9230ff12773dcbe0b9612f2
PixMate <94467160+pixmaate@users.noreply.github.com>
2023-11-29 22:28:56 +0100

    change between F and C

--- src/config.js
+++ src/config.js
@@ -7,25 +7,49 @@ class CurrentWeather {
         this.city = city;
     }
 
-    getWeather() {
+    getWeather(celsiusFarenheit) {
         fetch(`https://api.weatherapi.com/v1/current.json?key=${APIKEY}&q=${this.city}&aqi=no`, {mode: 'cors'})
         .then((response) => response.json())
         .then((response) => {
-          console.log(response);
-          const newReport = new CurrentWeatherDOMCelsius(response.current.temp_c,response.current.feelslike_c, response.current.condition.text, response.current.condition.icon, response.current.wind_kph, response.location.name)
+            if (celsiusFarenheit === 'C') {
+                const newReport = new CurrentWeatherDOM(response.current.temp_c,response.current.feelslike_c, response.current.condition.text, response.current.condition.icon, response.current.wind_kph, response.location.name, 'C')
+               // this.getNextDay('C');
+            } else if (celsiusFarenheit === 'F') {
+                const newReport = new CurrentWeatherDOM(response.current.temp_f,response.current.feelslike_f, response.current.condition.text, response.current.condition.icon, response.current.wind_mph, response.location.name, 'F')
+               // this.getNextDay('F');
+            };       
+        })
+        .catch((err) => {
+            console.log(err);
         });
     };
 
+    getNextDay(celsiusFarenheit) {
+        fetch(`https://api.weatherapi.com/v1/forecast.json?key=${APIKEY}&q=${this.city}&days=2&aqi=no`, {mode: 'cors'})
+        .then((response) => response.json())
+        .then((response) => {
+            if (celsiusFarenheit === 'C') {
+                const newTomo = new NextDayDOM(response.forecast[1].day.maxtemp_c, response.forecast[1].day.condition.text, response.forecast[1].day.condition.icon, response.forecast[1].day.maxwind_kph, 'C')
+            } else if (celsiusFarenheit === 'F') {
+                const newTomo = new NextDayDOM(response.forecast[1].day.maxtemp_f, response.forecast[1].day.condition.text, response.forecast[1].day.condition.icon, response.forecast[1].day.maxwind_mph, 'F')
+            }
+        })
+        .catch((err) => {
+            console.log(err);
+        });
+    }
+
 }
 
-class CurrentWeatherDOMCelsius {
-    constructor(tempC, feelsC, conditionText, iconSrc, windKPH, city) {
+class CurrentWeatherDOM {
+    constructor(tempC, feelsC, conditionText, iconSrc, windKPH, city, CorF) {
         this.tempC = tempC;
         this.feelsC = feelsC;
         this.conditionText = conditionText;
         this.iconSrc = iconSrc;
         this.windKPH = windKPH;
-        this.city = city
+        this.city = city;
+        this.CorF = CorF;
         this.buildDom();
     };
 
@@ -46,8 +70,17 @@ class CurrentWeatherDOMCelsius {
         weatherTemp.innerHTML = '';
         const tempActual = document.createElement('p');
         const tempFeels = document.createElement('p');
-        tempActual.textContent = `Current: ${this.tempC} Celsius`;
-        tempFeels.textContent = `Feels Like: ${this.feelsC} Celsius`;
+        if (this.CorF === 'C') {
+            tempActual.textContent = `${this.tempC} C`;
+            tempFeels.textContent = `Feels Like: ${this.feelsC} C`;
+        } else if (this.CorF === 'F') {
+            tempActual.textContent = `${this.tempC} F`;
+            tempFeels.textContent = `Feels Like: ${this.feelsC} F`;
+        }        
+        tempActual.style.cssText = 'font-size:1.3rem;font-weight:600;';
+       
+        weatherTemp.textContent = 'Now';
+        weatherTemp.style.cssText = 'font-size:1.1rem;';
         weatherTemp.appendChild(tempActual);
         weatherTemp.appendChild(tempFeels);
     };
@@ -60,6 +93,7 @@ class CurrentWeatherDOMCelsius {
 
         weatherConditionIcon.src = `https:${this.iconSrc}`;
         weatherConditionDescription.textContent = this.conditionText;
+        weatherConditionDescription.style.cssText = 'font-size:1.3rem;font-weight:600;';
         weatherCondition.appendChild(weatherConditionDescription);
         weatherCondition.appendChild(weatherConditionIcon);
 
@@ -68,19 +102,102 @@ class CurrentWeatherDOMCelsius {
     buildWind() {
         const weatherWind = document.querySelector('#wind');
         weatherWind.innerHTML = '';
-        weatherWind.textContent = `Wind: ${this.windKPH} KM/H`;
+        if (this.CorF === 'C') {
+            weatherWind.textContent = `Wind: ${this.windKPH} KM/H`;
+        } else if (this.CorF === 'F') {
+            weatherWind.textContent = `Wind: ${this.windKPH} MP/H`;
+        };        
     }
 };
+
+class NextDayDOM {
+    constructor(temp, conditionText, iconSrc, wind, CorF) {
+        this.temp = temp;
+        this.conditionText = conditionText;
+        this.iconSrc = iconSrc;
+        this.wind = wind;
+        this.CorF = CorF;
+        this.buildDom();
+    };
+
+    buildDom() {
+        this.buildTemperature();
+        this.buildCondition()
+        this.buildWind();        
+    };
+
+    
+
+    buildTemperature() {
+        const weatherTemp = document.querySelector('#temperatureTomo');
+        weatherTemp.innerHTML = '';
+        const tempActual = document.createElement('p');
+        if (this.CorF === 'C') {
+            tempActual.textContent = `${this.tempC} C`;
+        } else if (this.CorF === 'F') {
+            tempActual.textContent = `${this.tempC} F`;
+        }        
+        tempActual.style.cssText = 'font-size:1.3rem;font-weight:600;';
+       
+        weatherTemp.textContent = 'Tomorrow';
+        weatherTemp.style.cssText = 'font-size:1.1rem;';
+        weatherTemp.appendChild(tempActual);
+    };
+
+    buildCondition() {
+        const weatherCondition = document.querySelector('#conditionsTomo');
+        weatherCondition.innerHTML = '';
+        const weatherConditionDescription = document.createElement('p');
+        const weatherConditionIcon = document.createElement('img');
+
+        weatherConditionIcon.src = `https:${this.iconSrc}`;
+        weatherConditionDescription.textContent = this.conditionText;
+        weatherConditionDescription.style.cssText = 'font-size:1.3rem;font-weight:600;';
+        weatherCondition.appendChild(weatherConditionDescription);
+        weatherCondition.appendChild(weatherConditionIcon);
+
+    };
+
+    buildWind() {
+        const weatherWind = document.querySelector('#windTomo');
+        weatherWind.innerHTML = '';
+        if (this.CorF === 'C') {
+            weatherWind.textContent = `Wind: ${this.windKPH} KM/H`;
+        } else if (this.CorF === 'F') {
+            weatherWind.textContent = `Wind: ${this.windKPH} MP/H`;
+        };        
+    }
+
+}
 
 class SearchButton {
     constructor(selectedCity) {
         this.city = selectedCity;
         this.inputField = document.querySelector('#selected_city');
+        this.cfvalue = 'C';
         this.button = document.querySelector('#searchButton');
         this.button.addEventListener('click', (event) =>{
             event.preventDefault();
-            reportWeather(this.inputField.value);
+            reportWeather(this.inputField.value, this.cfvalue);
         })
+        this.selectorButton = document.querySelector('#c_or_f');
+        this.selectorListener();
+    }
+
+    selectorListener() {
+        this.selectorButton.addEventListener('click', (event) =>{
+            event.preventDefault();
+            if (this.selectorButton.textContent === 'F') {
+                this.cfvalue = 'F';
+                this.selectorButton.textContent = 'C';
+                reportWeather(this.inputField.value, this.cfvalue);
+            } else if (this.selectorButton.textContent === 'C') {
+                this.cfvalue = 'C';
+                this.selectorButton.textContent = 'F';
+                reportWeather(this.inputField.value, this.cfvalue);
+            };
+
+        });
     }
 
 
@@ -88,12 +205,12 @@ class SearchButton {
 
 }
 
-function reportWeather(selectedCity) {
+function reportWeather(selectedCity, CorF) {
     const weatherReport = new CurrentWeather(selectedCity);
-    const thisButton = new SearchButton(selectedCity);
-    weatherReport.getWeather();
+    weatherReport.getWeather(CorF);
 }
 
-reportWeather('Athens');
+const thisButton = new SearchButton('Athens');
+reportWeather('Athens', 'C');
     
 
